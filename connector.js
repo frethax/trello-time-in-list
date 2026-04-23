1e645031415a8a523b482f557603091b68cd5904
frethax <emirf@tcd.ie>
2026-04-23 16:58:21 +0100

    Update connector.js

--- connector.js
+++ connector.js
@@ -2,8 +2,6 @@
 
 var API_KEY = '526d48a7eb9050082ce280fe0ac1a67f';
 
-var DEFAULT_THRESHOLD_DAYS = 3;
-
 TrelloPowerUp.initialize({
 
   'card-back-section': function(t, options) {
@@ -11,53 +9,37 @@ TrelloPowerUp.initialize({
       title: 'Trello Time in List',
       icon:  'https://trello-time-in-list.vercel.app/icon.png',
       content: {
-        type: 'iframe',
-        url:  t.signUrl('https://trello-time-in-list.vercel.app/card-section.html'),
+        type:   'iframe',
+        url:    t.signUrl('https://trello-time-in-list.vercel.app/card-section.html'),
         height: 260
       }
     };
   },
 
   'card-badges': function(t, options) {
-    return t.get('board', 'shared', 'thresholdDays', DEFAULT_THRESHOLD_DAYS).then(function(thresholdDays) {
-      return t.card('id').then(function(card) {
+    return t.card('id').then(function(card) {
+      return fetch(
+        'https://api.trello.com/1/cards/' + card.id +
+        '/actions?filter=updateCard:idList&limit=1&key=' + API_KEY
+      )
+      .then(function(r) { return r.json(); })
+      .then(function(data) {
+        if (data && data.length) {
+          return makeBadge(data[0].date, t);
+        }
         return fetch(
           'https://api.trello.com/1/cards/' + card.id +
-          '/actions?filter=updateCard:idList&limit=1&key=' + API_KEY
-        ).then(function(r){ return r.json(); }).then(function(data) {
-          var lastMove;
-          if (data.length) {
-            lastMove = new Date(data[0].date);
-          } else {
-            return fetch(
-              'https://api.trello.com/1/cards/' + card.id +
-              '/actions?filter=createCard&limit=1&key=' + API_KEY
-            ).then(function(r){ return r.json(); }).then(function(cdata) {
-              if (!cdata.length) return [];
-              lastMove = new Date(cdata[0].date);
-              return buildBadge(lastMove, thresholdDays);
-            });
+          '/actions?filter=createCard&limit=1&key=' + API_KEY
+        )
+        .then(function(r) { return r.json(); })
+        .then(function(cdata) {
+          if (cdata && cdata.length) {
+            return makeBadge(cdata[0].date, t);
           }
-          return buildBadge(lastMove, thresholdDays);
+          return [];
         });
       });
     });
-  },
-
-  'board-buttons': function(t, options) {
-    return [{
-      text: 'Time in List Settings',
-      icon: 'https://trello-time-in-list.vercel.app/icon.png',
-      condition: 'admin',
-      callback: function(t) {
-        return t.get('board', 'shared', 'thresholdDays', DEFAULT_THRESHOLD_DAYS).then(function(current) {
-          var days = prompt('Red badge threshold (days):', current);
-          if (days && !isNaN(days)) {
-            return t.set('board', 'shared', 'thresholdDays', parseInt(days));
-          }
-        });
-      }
-    }];
   }
 
 }, {
@@ -65,13 +47,17 @@ TrelloPowerUp.initialize({
   appName: 'Trello Time in List'
 });
 
-function buildBadge(lastMove, thresholdDays) {
-  var diff = Date.now() - lastMove;
-  var isRed = diff > (thresholdDays * 24 * 60 * 60 * 1000);
-  return [{
-    text:  formatTime(diff),
-    color: isRed ? 'red' : 'green'
-  }];
+function makeBadge(dateStr, t) {
+  return t.get('board', 'shared', 'thresholdDays').then(function(val) {
+    var thresholdDays = (val && !isNaN(val)) ? parseInt(val) : 3;
+    var diff  = Date.now() - new Date(dateStr);
+    var isRed = diff > (thresholdDays * 24 * 60 * 60 * 1000);
+    return [{
+      text:    formatTime(diff),
+      color:   isRed ? 'red' : 'green',
+      refresh: 3600
+    }];
+  });
 }
 
 function formatTime(ms) {
