c9d6a48b83750946559ef5bdd4447cfed9301fde
frethax <emirf@tcd.ie>
2026-04-23 17:10:35 +0100

    Update connector.js

--- connector.js
+++ connector.js
@@ -14,59 +14,9 @@ TrelloPowerUp.initialize({
         height: 260
       }
     };
-  },
-
-  'card-badges': function(t, options) {
-    return t.card('id').then(function(card) {
-      return fetch(
-        'https://api.trello.com/1/cards/' + card.id +
-        '/actions?filter=updateCard:idList&limit=1&key=' + API_KEY
-      )
-      .then(function(r) { return r.json(); })
-      .then(function(data) {
-        if (data && data.length) {
-          return makeBadge(data[0].date, t);
-        }
-        return fetch(
-          'https://api.trello.com/1/cards/' + card.id +
-          '/actions?filter=createCard&limit=1&key=' + API_KEY
-        )
-        .then(function(r) { return r.json(); })
-        .then(function(cdata) {
-          if (cdata && cdata.length) {
-            return makeBadge(cdata[0].date, t);
-          }
-          return [];
-        });
-      });
-    });
   }
 
 }, {
   appKey:  API_KEY,
   appName: 'Trello Time in List'
 });
-
-function makeBadge(dateStr, t) {
-  return t.get('board', 'shared', 'thresholdDays').then(function(val) {
-    var thresholdDays = (val && !isNaN(val)) ? parseInt(val) : 3;
-    var diff  = Date.now() - new Date(dateStr);
-    var isRed = diff > (thresholdDays * 24 * 60 * 60 * 1000);
-    return [{
-      text:    formatTime(diff),
-      color:   isRed ? 'red' : 'green',
-      refresh: 3600
-    }];
-  });
-}
-
-function formatTime(ms) {
-  var minutes = Math.floor(ms / (1000 * 60));
-  var hours   = Math.floor(ms / (1000 * 60 * 60));
-  var days    = Math.floor(hours / 24);
-  var rest    = hours % 24;
-  if (minutes < 60) return minutes + 'm';
-  if (days > 0 && rest > 0) return days + 'd ' + rest + 'h';
-  if (days > 0) return days + 'd';
-  return hours + 'h';
-}
