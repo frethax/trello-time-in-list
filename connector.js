dec4b3210979b5230c91f71fa705ed2803171847
frethax <emirf@tcd.ie>
2026-05-04 15:13:43 +0100

    Update connector.js

--- connector.js
+++ connector.js
@@ -14,6 +14,36 @@ TrelloPowerUp.initialize({
         height: 260
       }
     };
+  },
+
+  'card-badges': function(t, options) {
+    var restApi = t.getRestApi();
+    return restApi.getToken()
+      .then(function(token) {
+        if (!token) return [];
+        return t.card('id').then(function(card) {
+          return fetch(
+            'https://api.trello.com/1/cards/' + card.id +
+            '/actions?filter=updateCard:idList&limit=1&key=' + API_KEY + '&token=' + token
+          )
+          .then(function(r) { return r.json(); })
+          .then(function(data) {
+            if (data && data.length) {
+              return makeBadge(data[0].date, t, token, card.id);
+            }
+            return fetch(
+              'https://api.trello.com/1/cards/' + card.id +
+              '/actions?filter=createCard&limit=1&key=' + API_KEY + '&token=' + token
+            )
+            .then(function(r) { return r.json(); })
+            .then(function(cdata) {
+              if (cdata && cdata.length) return makeBadge(cdata[0].date, t, token, card.id);
+              return [];
+            });
+          });
+        });
+      })
+      .catch(function() { return []; });
   }
 
 }, {
@@ -24,3 +54,27 @@ TrelloPowerUp.initialize({
   authorizeButton: true,
   scope: { read: true }
 });
+
+function makeBadge(dateStr, t, token, cardId) {
+  return t.get('board', 'shared', 'thresholdDays').then(function(val) {
+    var thresholdDays = (val && !isNaN(val)) ? parseInt(val) : 3;
+    var diff  = Date.now() - new Date(dateStr);
+    var isRed = diff > (thresholdDays * 24 * 60 * 60 * 1000);
+    return [{
+      text:    formatTime(diff),
+      color:   isRed ? 'red' : 'green',
+      refresh: 3600
+    }];
+  });
+}
+
+function formatTime(ms) {
+  var minutes = Math.floor(ms / (1000 * 60));
+  var hours   = Math.floor(ms / (1000 * 60 * 60));
+  var days    = Math.floor(hours / 24);
+  var rest    = hours % 24;
+  if (minutes < 60) return minutes + 'm';
+  if (days > 0 && rest > 0) return days + 'd ' + rest + 'h';
+  if (days > 0) return days + 'd';
+  return hours + 'h';
+}
