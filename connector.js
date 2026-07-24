e93bd76aed97faa0f9353849ef1afc8990c350ab
frethax <emirf@tcd.ie>
2026-07-25 00:32:28 +0100

    Increase modal height from 600 to 680

--- connector.js
+++ connector.js
@@ -77,7 +77,7 @@ TrelloPowerUp.initialize({
         return t.modal({
           url:    t.signUrl('https://trello-time-in-list.vercel.app/settings.html'),
           accentColor: '#0052cc',
-          height: 600,
+          height: 680,
           fullscreen: false,
           title: 'ListClock Settings'
         });
